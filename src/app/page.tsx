65c27ea69156cadbd97dec4bdc0a435d83b36f6a
Bartek Kochanowicz <bkochanovitz@gmail.com>
2024-07-14 18:40:32 +0200

    homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { type Metadata } from "next";
+import { H1 } from "@/components/Heading";
 
 export const metadata: Metadata = {
 	title: "Homepage | Next ecommerce store",
@@ -6,7 +7,28 @@ export const metadata: Metadata = {
 };
 
 const HomePage = () => {
-	return <div>homepage</div>;
+	return (
+		<>
+			<article className="mt-6 w-full bg-foreground py-10 text-white">
+				<H1 className="text-center text-6xl">SALE</H1>
+				<p className="text-center">up to 90%</p>
+			</article>
+			<section className="my-12 grid grid-cols-1 gap-6 md:grid-cols-3">
+				<div className="text-center text-sm hover:underline">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nunc at aliquam
+					tincidunt, nunc nunc ultricies nunc, nec lacinia nunc nunc at mauris.
+				</div>
+				<div className="text-center text-sm hover:underline">
+					Sed euismod, nunc at aliquam tincidunt, nunc nunc ultricies nunc, nec lacinia nunc nunc at
+					mauris.
+				</div>
+				<div className="text-center text-sm hover:underline">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nunc at aliquam
+					tincidunt, nunc nunc ultricies nunc, nec lacinia nunc nunc at mauris.
+				</div>
+			</section>
+		</>
+	);
 };
 
 export default HomePage;
