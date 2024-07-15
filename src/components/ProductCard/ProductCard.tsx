12e40be5eb70db1bf1271fb578889dd06dc10750
Bartek Kochanowicz <bkochanovitz@gmail.com>
2024-07-15 21:00:09 +0200

    update heading size

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -21,30 +21,28 @@ export const ProductCard = async ({
 	rating,
 	price,
 	id,
-}: Product) => {
-	// const blurData = await getBase64(image);
-
-	return (
-		<Link href={`${ROUTES.PRODUCT}/${id}`}>
-			<Card className="group transition hover:border-neutral-400">
-				<CardHeader>
-					<CardTitle className="sm:h-12 sm:overflow-hidden">{title}</CardTitle>
-					<CardDescription className="capitalize">{category}</CardDescription>
-				</CardHeader>
-				<CardContent>
-					<ResponsiveRemoteImage
-						className="h-60"
-						sizes="(min-width: 1280px) 248px, (min-width: 1040px) calc(33.18vw - 72px), (min-width: 640px) calc(50vw - 78px), calc(100vw - 90px)"
-						imageClassName="transition duration-500 group-hover:scale-105"
-						src={image}
-						alt={description}
-					/>
-				</CardContent>
-				<CardFooter className="flex items-center justify-between">
-					<span className="text-lg font-bold">{formatPrice(price)}</span>
-					<ProductRate {...rating} />
-				</CardFooter>
-			</Card>
-		</Link>
-	);
-};
+}: Product) => (
+	<Link href={`${ROUTES.PRODUCT}/${id}`}>
+		<Card className="group transition hover:border-neutral-400">
+			<CardHeader>
+				<CardTitle as="h2" className="sm:h-12 sm:overflow-hidden">
+					{title}
+				</CardTitle>
+				<CardDescription className="capitalize">{category}</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<ResponsiveRemoteImage
+					className="h-60"
+					sizes="(min-width: 1280px) 248px, (min-width: 1040px) calc(33.18vw - 72px), (min-width: 640px) calc(50vw - 78px), calc(100vw - 90px)"
+					imageClassName="transition duration-500 group-hover:scale-105"
+					src={image}
+					alt={description}
+				/>
+			</CardContent>
+			<CardFooter className="flex items-center justify-between">
+				<span className="text-lg font-bold">{formatPrice(price)}</span>
+				<ProductRate {...rating} />
+			</CardFooter>
+		</Card>
+	</Link>
+);
